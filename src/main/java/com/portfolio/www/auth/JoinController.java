bf27762c9711fb1bdff378bde33d62c19de3c58d
JaeYeol Lee <jyeory@gsretail.com>
2024-04-11 22:19:03 +0900

    first commit

---
+++ src/main/java/com/portfolio/www/auth/JoinController.java
@@ -0,0 +1,24 @@
+package com.portfolio.www.auth;
+
+import java.util.Calendar;
+import java.util.HashMap;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class JoinController {
+	
+	
+	@RequestMapping("/auth/joinPage.do")
+	public ModelAndView joinPage(@RequestParam HashMap<String, String> params) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("key", Calendar.getInstance().getTimeInMillis());
+		mv.setViewName("auth/join");
+		
+		return mv;
+	}
+
+}
